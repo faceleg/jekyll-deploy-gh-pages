b4949b419f82d3e2eda19b7bf006da50aa8614b4
Michael Robinson <mike@pagesofinterest.net>
2023-11-20 20:15:45 +1300

    Some more debug information

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,14 @@ bundle list | grep "jekyll ("
 echo '👍 BUNDLE INSTALLED—BUILDING THE SITE'
 bundle exec jekyll build
 echo '👍 THE SITE IS BUILT—PUSHING IT BACK TO GITHUB-PAGES'
+
+echo "ls -lha"
+ls -lha
 cd build
+
+echo "pwd"
+pwd
+
 remote_repo="https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" &&
 	remote_branch="gh-pages" &&
 	git config --global init.defaultBranch main &&
@@ -21,4 +28,7 @@ remote_repo="https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITO
 	git push --force $remote_repo main:$remote_branch >/dev/null 2>&1 &&
 	rm -fr .git &&
 	cd ../
+
+echo "	git push --force $remote_repo main:$remote_branch >/dev/null 2>&1 &&"
+
 echo '👍 GREAT SUCCESS!'
